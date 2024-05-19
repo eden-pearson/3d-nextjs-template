5a22e990a26e03a377cce5834af438e93423d255
Eliáš Bauer <zenbauhaus@gmail.com>
2024-05-19 15:08:16 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -29,9 +29,9 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
         <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
+          <p className='w-full uppercase'>art&tech polymath</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>zenbauhaus</h1>
+          <p className='mb-8 text-2xl leading-normal'>lifelong learner driven by boundless curiosity</p>
         </div>
 
         <div className='w-full text-center md:w-3/5'>
@@ -47,8 +47,8 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         {/* first row */}
         <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
-          <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>with a profound passsion</h2>
+          <p className='mb-8 text-gray-600'>for the synergy of art and technology</p>
         </div>
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
           <View orbit className='relative h-full  sm:h-48 sm:w-full'>
@@ -68,12 +68,13 @@ export default function Page() {
           </View>
         </div>
         <div className='w-full p-6 sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>dedicated to being a bridge between creative vision and technical execution</h2>
           <p className='mb-8 text-gray-600'>
-            3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
-            tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
-            have multiple views with a single, performant canvas. These views will follow their tracking elements,
-            scroll along, resize, etc.
+            whether supporting artists in making their dreams tangible or spicing up a simple technical query with a fresh idea, 
+            my heart lies at the crossroads of imagination and innovation.
+            fortunate to work with both artistic and technical minds, continually learning the importance of functionality, aesthetics and user experience.
+            every collaboration is an opportunity to unleash visionary creativity and state of the art technology.
+            let's collaborate and bring new visions to life together!
           </p>
         </div>
       </div>
